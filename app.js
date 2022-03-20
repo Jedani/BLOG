3cbe9ea5ebe646a3d14ee710a182cebbabaa081d
Allan <allansichangi35@gmail.com>
2022-03-21 01:10:39 +0300

    skeletal start

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ const app = express();
 
 const port = "3020";
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-	res.send("<h1>hello</h1>");
+	res.render("index");
+});
+
+app.get("/signup", (req, res) => {
+	res.render("signup");
+});
+
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+app.get("/blogs", (req, res) => {
+	res.render("blogs");
+});
+
+app.use((req, res) => {
+	res.status(404).render("404");
 });
 
 app.listen(port, () => {
