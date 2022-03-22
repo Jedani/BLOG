f611a0802f1c74598b8ef86a299a2b4f8db8f0e6
Allan <allansichangi35@gmail.com>
2022-03-22 06:57:44 +0300

    skeletal structure

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const mysql = require("mysql");
 const express = require("express");
 const app = express();
 
@@ -6,8 +7,30 @@ const port = "3020";
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+
+let connection = mysql.createConnection({
+	host: "localhost",
+	user: "fich",
+	password: "forever",
+	database: "join_us",
+});
+
+let blogs = [];
+
+connection.query(
+	"SELECT full_name FROM users LIMIT 10",
+	(error, results, field) => {
+		if (error) throw error;
+		for (i = 0; i < results.length; i++) {
+			blogs.push(results[i].full_name);
+		}
+	},
+);
+
+connection.end;
+
 app.get("/", (req, res) => {
-	res.render("index");
+	res.render("index", { title: "Home", blogs });
 });
 
 app.get("/signup", (req, res) => {
