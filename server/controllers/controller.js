f09915db6c00718c33ec2546143eff49c99f5b18
Allan <allansichangi35@gmail.com>
2022-03-24 23:50:35 +0300

    skeletal structure

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,17 +1,43 @@
 const db = require("../model/database");
 // blogs_page, blog_index, login_page, create_page, about_page, sign_up, delete_blog
 
-const blog_index = async (req, res) => {
+const blogs_page = async (req, res) => {
+	let blogs = [];
 	try {
-		let blogs = await db.all();
-		console.log(blogs[0].username);
-		res.render("index", { title: "Home", blogs });
+		let blog = await db.all();
+		for (let i = 0; i < blog.length; i++) {
+			blogs.push(blog[i].username);
+		}
+		res.render("blogs/blogs", { title: "blogs", blogs });
 	} catch (e) {
-		console.log(e);
-		res.sendStatus(500);
+		res.sendStatus(500).render(e);
 	}
+};
+
+const blog_index = async (req, res) => {
+	let blogs = [];
+	try {
+		let blog = await db.all();
+		for (let i = 0; i < blog.length; i++) {
+			blogs.push(blog[i].username);
+		}
+		res.render("blogs/index", { title: "Home", blogs });
+	} catch (e) {
+		res.sendStatus(500).render(e);
+	}
+};
+
+const login_page = (req, res) => {
+	res.render("login");
+};
+
+const sign_up = (req, res) => {
+	res.render("signup");
 };
 
 module.exports = {
 	blog_index,
+	blogs_page,
+	login_page,
+	sign_up,
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,17 +4,8 @@ const blogController = require("../controllers/controller");
 const router = express.Router();
 
 router.get("/", blogController.blog_index);
-
-router.get("/signup", (req, res) => {
-	res.render("signup");
-});
-
-router.get("/login", (req, res) => {
-	res.render("login");
-});
-
-router.get("/blogs", (req, res) => {
-	res.render("blogs");
-});
+router.get("/blogs", blogController.blogs_page);
+router.get("/login", blogController.login_page);
+router.get("/signup", blogController.sign_up);
 
 module.exports = router;
